3ab54a5a81a33404043d12d538e9a6f0e74ee135
Alex Albaran <albaranlex@icloud.com>
2021-01-05 00:18:31 +0100

    04.01 Homework

---
+++ jan/04.01/index.js
@@ -0,0 +1,41 @@
+//1
+var move = "I can walk in the park all day."
+console.log(move);
+
+//2
+var greeting = "Hello World!"
+var greetingUpperCase = greeting.toUpperCase;
+console.log(greetingUpperCase);
+
+//3
+var race = "Earthlings";
+var raceLowerCase = race.toLowerCase;
+console.log(raceLowerCase);
+
+//4
+var file = "JavaScript";
+var middleOf = file.substring(3, 6);
+console.log(middleOf);
+
+//5
+var shoes = "nice shoes";
+var police = shoes.includes('l');
+console.log(police);
+
+//6
+var engineer = file[0] + file + file[0];
+console.log(engineer);
+
+//7
+var builder = file.substring(7,10) + file + file.substring(7, 10);
+console.log(builder);
+
+//8
+var color = "Orange";
+var colorUpperCase = color.toUpperCase;
+var checkPoint = color.includes('Java');
+console.log(colorUpperCase , checkPoint);
+
+// For Number 9, i thought strings are immutable?//
+
+
