54484f2598e9aba5e5f17eca2bb9c34a252de041
Alex Albaran <albaranlex@icloud.com>
2021-01-05 11:33:37 +0100

    Short Activity

---
+++ jan/05.01/index.js
@@ -0,0 +1,15 @@
+//concatenation
+let var1 = "Hello";
+let var2 = 2;
+let sum = var1 + " " + var2;
+console.log(sum);
+
+//check if number is odd or even
+let var3 = 5;
+let var4 = 40;
+let var5 = 2.1;
+let var6 = 22;
+
+var6 % 2 == 0
+  ? console.log("This is even")
+  : console.log("That is odd!");
