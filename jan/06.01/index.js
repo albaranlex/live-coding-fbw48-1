54c2d7bf4e24a44112da8883b89652342425d70a
Alex Albaran <albaranlex@icloud.com>
2021-01-06 22:47:31 +0100

    Holiday Homework 06.01.21

---
+++ jan/06.01/index.js
@@ -0,0 +1,46 @@
+//Min and Max
+console.log(Math.min(-1, 1, 2, 3, 4));
+console.log(Math.max(-1, 1, 2, 3, 4));
+
+//Rounding Up
+let flt1 = 3321.32321;
+let flt2 = 326.76;
+let flt3 = 76788.7;
+let flt4 = -9.78;
+let flt5 = 43.342;
+
+console.log(Math.ceil(flt1));
+console.log(Math.ceil(flt2));
+console.log(Math.ceil(flt3));
+console.log(Math.ceil(flt4));
+console.log(Math.ceil(flt5));
+
+//Round Down
+let flt6 = 3321.32321;
+let flt7 = 326.76;
+let flt8 = 76788.7;
+let flt9 = -9.78;
+let flt10 = 28.329;
+
+console.log(Math.floor(flt1));
+console.log(Math.floor(flt2));
+console.log(Math.floor(flt3));
+console.log(Math.floor(flt4));
+console.log(Math.floor(flt5));
+
+//Concatenation
+let intro = "Hi, my name is Alex and I am ";
+let age = 25;
+console.log(intro + 25);
+
+//What is the "+" operator doing? It's concatenating two variables. The output is always a string however and idk why.
+
+//Converting
+let str1 = "1005";
+console.log(parseInt(str1, 10));
+
+let num1 = "10.05";
+console.log(parseFloat(num1));
+
+
+
