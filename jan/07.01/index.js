25732210b62d4845ac1821f28e1e8695c5bd8d50
Alex Albaran <albaranlex@icloud.com>
2021-01-07 11:53:13 +0100

    Short Exercises

---
+++ jan/07.01/index.js
@@ -0,0 +1,9 @@
+//Random Integer between 1-6
+console.log(Math.floor(Math.random() * 6) + 1);
+
+//Random Numbers between 20 - 22
+console.log(Math.floor(Math.random() * 3) + 20);
+
+//Random Numbers between 0 - 10
+console.log(Math.floor(Math.random() * 10) + 1);
+
