0ad398ee6dba400f011ee87229c636f646418e03
Alex Albaran <albaranlex@icloud.com>
2021-02-16 13:35:50 +0100

    Method Three

--- feb/16.02.js
+++ feb/16.02.js
@@ -3,6 +3,12 @@
 // Create a program that adds up the numbers in an array (of at least 3 numbers).
 //Bonus: Print to screen both the sum and the product of these numbers.
 
-const sumOfNumbers = (...arr) => arr.reduce((acc, cur) => acc + cur);
+const sumOfNumbers = (...arr) => {
+  let total = 0;
+  for (let num in arr) {
+    total += arr[num];
+  }
+  return total;
+};
 
 console.log(sumOfNumbers(1, 2, 3, 4));
