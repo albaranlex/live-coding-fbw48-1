e6754036bcb5c6b868afda6b82ece7e697ed5750
Alex Albaran <albaranlex@icloud.com>
2021-02-16 15:15:18 +0100

    Problem 3 with 3 Methods

--- feb/16.02.js
+++ feb/16.02.js
@@ -1,14 +1,30 @@
-// SumOfNumbers.
+//##### 3. Word Converter
+//- Given an array of words, create a function named "_wordConverter_" which uses an array method to add _"er"_ to the end of each string in the array. Look at the expected output below. **Important**: Do **not** use a loop to complete this task. Print your solution to the console.
 
-// Create a program that adds up the numbers in an array (of at least 3 numbers).
-//Bonus: Print to screen both the sum and the product of these numbers.
+const wordArr = ["smart", "kind", "sweet", "small", "clear"];
 
-const sumOfNumbers = (...arr) => {
-  let total = 0;
-  for (let num in arr) {
-    total += arr[num];
+//Method One
+const arr2 = ["smart", "kind", "sweet", "small", "clear"];
+const wordConverter1 = (arr) => arr.map((item) => item + "er");
+console.log(wordConverter1(arr2));
+
+//Method 2
+const wordConverter2 = (arr) => {
+  let result = [];
+  for (let i = 0; i < arr.length; i++) {
+    result += arr[i] + "er" + " ";
   }
-  return total;
+  return result;
+};
+console.log(wordConverter2(wordArr));
+
+//Method 3
+const wordConverter3 = (arr) => {
+  let result = [];
+  for (let word in arr) {
+    result += arr[word] + "er" + " ";
+  }
+  return result;
 };
 
-console.log(sumOfNumbers(1, 2, 3, 4));
+console.log(wordConverter3(wordArr));
