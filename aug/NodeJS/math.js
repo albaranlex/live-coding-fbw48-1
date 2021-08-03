395c28b33016890605c543a4831be80f8e50a12f
Alex Albaran <albaranlex@icloud.com>
2021-08-03 10:38:06 +0200

    module.exports

---
+++ aug/NodeJS/math.js
@@ -0,0 +1,12 @@
+const add = (x, y) => x +y;
+const square = (x) => x * x;
+
+const math = {
+    add: add,
+    square: square
+}
+
+// module.exports.add = add;
+// module.exports.square = square;
+module.exports = math;
+
